eea547b79d9680019590fea2cd41b37efa82e042
lightjedi5 <jerryplatero@hotmail.com>
2020-05-07 19:01:03 -0600

    added section 7-9

--- Udemy/CourseWork/controllers/products.js
+++ Udemy/CourseWork/controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('add-product', {
+  res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     formsCSS: true,
@@ -16,15 +16,4 @@ exports.postAddProduct = (req, res, next) => {
   res.redirect('/');
 };
 
-exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
-    });
-  });
-};
+
